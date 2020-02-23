d626acf033f0a08a0c572096967ae2c7d6a55304
HKlee057 <45294682+HKlee057@users.noreply.github.com>
2020-02-22 22:39:46 -0600

    Inicio de ADC Slave
    Palabra de configuración y creación de librerías pertinentes

---
+++ SlaveADC_Lab_5.X/build/default/production/ADC_Slave.p1.d
@@ -0,0 +1,4 @@
+build/default/production/ADC_Slave.p1:  \
+ADC_Slave.c  \
+Oscilador.h  \
+ADC.h 
---
+++ SlaveADC_Lab_5.X/build/default/production/_ext/155366161/Oscilador.p1.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/155366161/Oscilador.p1:  \
+../SlaveLED_Lab_5.X/Oscilador.c  \
+../SlaveLED_Lab_5.X/Oscilador.h 
